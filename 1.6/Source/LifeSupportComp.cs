4c370e32723348d387495f3f6bbd2f2592933e06
kittycat2002 <josh.masters.02@gmail.com>
2025-07-23 23:11:43 -0400

    Fixed gravship usage

--- 1.6/Source/LifeSupportComp.cs
+++ 1.6/Source/LifeSupportComp.cs
@@ -9,36 +9,50 @@ namespace LifeSupport;
 /// <summary>
 /// Tags this Thing as being valid life support.
 /// </summary>
-public class LifeSupportComp : ThingComp {
-    public bool Active => parent.TryGetComp<CompPowerTrader>() is not { } power || power.PowerOn;
+public class LifeSupportComp : ThingComp
+{
+	public bool Active => parent.TryGetComp<CompPowerTrader>() is not { } power || power.PowerOn;
 
-    public override void ReceiveCompSignal(string signal) {
-        if (signal is not ("PowerTurnedOn" or "PowerTurnedOff")) {
-            return;
-        }
-            
-        //Check for state change in surrounding pawns in beds.
-        Map map = parent.Map;
-        List<Pawn> pawns = parent.CellsAdjacent8WayAndInside()
-            .SelectMany(cell => cell.GetThingList(map).OfType<Building_Bed>().SelectMany(bed => bed.CurOccupants))
-            .Where(pawn => !pawn.health.Dead)
-            .Distinct()
-            .ToList();
-            
-        foreach (Pawn pawn in pawns) {
-            LifeSupportUtility.SetHediffs(pawn);
-        }
-    }
-    public override void PostDestroy(DestroyMode mode, Map previousMap)
-    {
-        List<Pawn> pawns = parent.CellsAdjacent8WayAndInside()
-            .SelectMany(cell => cell.GetThingList(previousMap).OfType<Building_Bed>().SelectMany(bed => bed.CurOccupants))
-            .Where(pawn => !pawn.health.Dead)
-            .Distinct()
-            .ToList();
-            
-        foreach (Pawn pawn in pawns) {
-            LifeSupportUtility.SetHediffs(pawn, false);
-        }
-    }
+	public override void ReceiveCompSignal(string signal)
+	{
+		if (signal is not ("PowerTurnedOn" or "PowerTurnedOff") || parent.Map == null)
+		{
+			return;
+		}
+
+		//Check for state change in surrounding pawns in beds.
+		List<Pawn> pawns = parent.CellsAdjacent8WayAndInside()
+			.SelectMany(cell =>
+				cell.GetThingList(parent.Map).OfType<Building_Bed>().SelectMany(bed => bed.CurOccupants))
+			.Where(pawn => !pawn.health.Dead)
+			.Distinct()
+			.ToList();
+
+		foreach (Pawn pawn in pawns)
+		{
+			LifeSupportUtility.SetHediffs(pawn);
+		}
+	}
+
+	public override void PostDeSpawn(Map map, DestroyMode mode = DestroyMode.Vanish)
+	{
+		if (mode == DestroyMode.WillReplace)
+		{
+			base.PostDeSpawn(map, mode);
+			return;
+		}
+
+		List<Pawn> pawns = parent.CellsAdjacent8WayAndInside()
+			.SelectMany(cell => cell.GetThingList(map).OfType<Building_Bed>().SelectMany(bed => bed.CurOccupants))
+			.Where(pawn => !pawn.health.Dead)
+			.Distinct()
+			.ToList();
+
+		foreach (Pawn pawn in pawns)
+		{
+			LifeSupportUtility.SetHediffs(pawn, false);
+		}
+
+		base.PostDeSpawn(map, mode);
+	}
 }
